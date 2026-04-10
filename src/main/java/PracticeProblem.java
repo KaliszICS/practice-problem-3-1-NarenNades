41fffde26bdfa3228344f2341e5dcbc5971e1d50
NarenNades <344941885@tdsb.ca>
2026-04-10 15:08:22 +0000

    Done!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,38 @@
+/**
+ * File: If statements
+ * Author: Naren Nades
+ * Date Created: Mar 23, 2026
+ * Date Last Modified: Apr 10, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+
+	public static String evenOrOdd(int num) {
+		if (num % 2 == 0){
+			return "Even";
+		}
+		return "Odd";
+	}
+	
+	public static String teacherOrStudent(String name) {
+		if (name == "Kalisz") {
+			return "Teacher";
+		}
+		return "Student";
+	}
+
+	public static int fartherFromZero(int num) {
+		if (num > 0) {
+			return num + 5;
+		}
+		if (num < 0) {
+			return num - 5;
+		}
+		return num;
 	}
 
 }
